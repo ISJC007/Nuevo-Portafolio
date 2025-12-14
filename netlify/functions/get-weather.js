00584117f67494684a949c05d83578da9217098e
Tu Nombre Completo <johandercedenocr16@gmail.com>
2025-12-14 04:08:42 -0400

    Refactor: Aplicando SRP y separando lógica del simulador

--- netlify/functions/get-weather.js
+++ netlify/functions/get-weather.js
@@ -1,22 +1,22 @@
 exports.handler = async (event) => {
-
-  
+    
     const API_KEY = process.env.OPENWEATHER_API_KEY; 
+    const city = event.queryStringParameters.city || 'Caracas';
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
 
-  const city = event.queryStringParameters.city || 'Caracas';
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
-  try {
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    console.error("Error en la función Netlify:", error);
-    return { statusCode: 500, body: JSON.stringify({ error: 'Fallo al obtener datos del clima.' }) };
-  }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        console.error("Error en la función Netlify:", error);
+        return { 
+            statusCode: 500, 
+            body: JSON.stringify({ error: 'Fallo al obtener datos del clima.' }) 
+        };
+    }
 };
